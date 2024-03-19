bd022bd8366517616869c9aa49b2d05f1e3c7e57
Jakub Jurysek <jakubjurysek@astronomie,cz>
2024-03-19 13:50:37 +0100

    change sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,19 @@ release = sst1mpipe.__version__
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    'sphinx.ext.githubpages', 
+    "numpydoc",
+    "nbsphinx",
+    "sphinx_automodapi.automodapi",
+    "sphinxarg.ext",
+    "sphinxcontrib.mermaid",
+    "sphinx_togglebutton",
+    "sphinx_rtd_theme"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,6 +38,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#import sphinx_rtd_theme
-html_theme = 'classic'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
