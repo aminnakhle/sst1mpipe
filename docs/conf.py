fa0fa1140860abbc7875f2464e1db326ea3ec0cb
Jakub Jurysek <jakubjurysek@astronomie,cz>
2024-03-19 13:54:38 +0100

    change sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
