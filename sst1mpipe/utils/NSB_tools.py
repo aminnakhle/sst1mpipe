5e92980c28e49ede242efb1e6ed28ce69c22c50e
Thomas Tavernier <tavernier@calculus.sst1m>
2024-07-22 17:11:09 +0200

    NSB tools update

--- sst1mpipe/utils/NSB_tools.py
+++ sst1mpipe/utils/NSB_tools.py
@@ -6,7 +6,13 @@ Created Feb 29 2024
 
 import numpy as np
 import astropy.units as u
+from astropy.time import Time
+from ctapipe.io import read_table
+
+import astropy
 from scipy import interpolate
+
+import matplotlib.pyplot as plt 
 
 ## spline aprox NSB : VAR[ADC]->SHIFT[ADC]
 ## these are rough aprox.. To be updated!
@@ -28,7 +34,21 @@ TCK2=(np.array([0.        ,   0.        ,  93.74735497, 126.46232902,
                   0.        ]),
       1)
 
+BINWIDTH = 4e-3 ## micro second
 
+#we assume drops VS BLS (Baseline shift) to be linear :
+# these numbers come from fig. 11.1 in "SiPM behaviour under continuous light"
+# https://arxiv.org/abs/1910.00348
+Vdrop_lin_aprox = dict({'gain_tel21': -0.00487,
+                        'gain_tel22': -0.00118,
+                        'PDE_tel21' : -0.00317,
+                        'PDE_tel22' : -0.000663,
+                        'xt_tel21'  : -0.00768,
+                        'xt_tel22'  : -0.00207 })
+#Sipm gain w/o Vdrop
+## total pulse intergal 
+Gain_0 = dict({'tel21': 21.0,
+               'tel22': 23.5})
 
 
 def VAR_to_shift(baseline_VAR,ntel):
@@ -41,71 +61,147 @@ def VAR_to_shift(baseline_VAR,ntel):
         shift=0
     return shift
 
-## linear aproximation of the gain+XT+PDE variation 
-## as a function of the baseline shift
+def NSB_to_BLS(NSB_MHz,ntel):
+    A = NSB_MHz*Gain_0['tel{}'.format(ntel)]*BINWIDTH
+    return A/(1-Vdrop_lin_aprox['gain_tel{}'.format(ntel)]*A)
 
-def shift_to_I_modifier_t1(B_shift,slope = -0.0077):
-    return slope*B_shift+1
-
-def shift_to_I_modifier_t2(B_shift,slope = -0.0019):
-    return slope*B_shift+1
-
+def make_drop_func(param_name,itel):
+    
+    def drop_func(B_shift):
+        slope   = Vdrop_lin_aprox[param_name+'_tel{}'.format(ntel)]
+        return max(slope*B_shift+1, 0)
+    return drop_func
 
 def VAR_to_Idrop(baseline_VAR,ntel):
+    ## X-talk not included yet
     ## Usage :
     ## I_corr = I / I_drop
-    if ntel==21:
-        shift = interpolate.splev(baseline_VAR, TCK1, der=0)
-        I_drop = shift_to_I_modifier_t1(shift)
-    elif ntel==22:
-        shift = interpolate.splev(baseline_VAR, TCK2, der=0)
-        I_drop = shift_to_I_modifier_t2(shift)
-    else:
+    try:
+        slope   = Vdrop_lin_aprox['gain_tel{}'.format(ntel)]+ \
+                  Vdrop_lin_aprox['PDE_tel{}'.format(ntel)]
+    except:
         print("ERROR ntel should be 21 or 22")
-    return I_drop
+    return VAR_to_shift(baseline_VAR,ntel)*slope+1
 
-def get_optical_eff_shift(ntel):
+def VAR_to_Gdrop(baseline_VAR,ntel):
     ## Usage :
-    ## I_corr = I * optical_eff_shift
-
-    if ntel==21:
-        optical_eff_shift = 0.96
-
-    elif ntel==22:
-        optical_eff_shift = 1.1
-
-    else:
+    ## G_corr = G / G_drop
+    try:
+        slope   = Vdrop_lin_aprox['gain_tel{}'.format(ntel)]
+    except:
         print("ERROR ntel should be 21 or 22")
-    return optical_eff_shift 
+
+    return VAR_to_shift(baseline_VAR,ntel)*slope+1
 
 
-def get_simple_nsbrate(bs_shift,dc_to_pe,binlenght=4*u.ns):
-    rate = bs_shift/dc_to_pe / binlenght
+def get_simple_nsbrate(bs_shift,gain,binlenght=4*u.ns):
+    rate = bs_shift/gain / binlenght
     return rate.to('MHz')
 
 def gain_drop_th(nsb_rate, cell_capacitance=85. * u.fF, bias_resistance=2.4 * u.kohm):
     return 1 - 1 / (1 + (nsb_rate * cell_capacitance * bias_resistance).to_value(1))
 
-def VAR_to_NSB(baseline_VAR,ntel,dc_to_pe=None):
-    if (ntel==21) and (dc_to_pe==None):
-        no_nsb_f = 21.8
-    elif (ntel==22) and (dc_to_pe==None):
-        no_nsb_f = 25.5
-    else:
-        no_nsb_f=dc_to_pe
-    NSB_rate = get_simple_nsbrate(VAR_to_shift(baseline_VAR,ntel),
-                                  no_nsb_f * VAR_to_Idrop(baseline_VAR,ntel))
-    return NSB_rate
+
     
-def shift_to_NSB(baseline_shift,ntel,dc_to_pe=None):
-    if (ntel==21) and (dc_to_pe==None):
-        no_nsb_f = 21.8
-    elif (ntel==22) and (dc_to_pe==None):
-        no_nsb_f = 25.5
-    else:
-        no_nsb_f=dc_to_pe
+def BLS_to_NSB(baseline_shift,ntel,gain=None):
+    if gain is None:
+        gain = Gain_0['tel{}'.format(ntel)]
+    Gdrop = baseline_shift * Vdrop_lin_aprox['gain_tel{}'.format(ntel)]+1
     NSB_rate = get_simple_nsbrate(baseline_shift,
-                                  no_nsb_f * VAR_to_Idrop(baseline_VAR,ntel))
+                                  gain * Gdrop)
     return NSB_rate
+
+def BLS_to_NSB_photon_rate(baseline_shift,ntel,gain=None):
+    PDE_0 = 0.35 ## aprox. TODO : get some better estimation
+    if gain is None:
+        gain = Gain_0['tel{}'.format(ntel)]
+
+    slope   = Vdrop_lin_aprox['gain_tel{}'.format(ntel)]+ \
+              Vdrop_lin_aprox['PDE_tel{}'.format(ntel)]
+
+    Idrop = baseline_shift*slope+1
+    NSB_rate = get_simple_nsbrate(baseline_shift,
+                                  gain * Idrop) / PDE_0
+
+    return NSB_rate
+
+def VAR_to_NSB(baseline_VAR,ntel,gain=None):
+    if gain is None:
+        gain = Gain_0['tel{}'.format(ntel)]
+    NSB_rate = BLS_to_NSB(VAR_to_shift(baseline_VAR,ntel),
+                          ntel,
+                          gain=gain)
+    return NSB_rate
+
+def VAR_to_NSB_photon_rate(baseline_VAR,ntel,gain=None):
+    if gain is None:
+        gain = Gain_0['tel{}'.format(ntel)]
+    NSB_rate = BLS_to_NSB_photon_rate(VAR_to_shift(baseline_VAR,ntel),
+                                      ntel,
+                                      gain=gain)
+    return NSB_rate
+
+############
+ #### tools to plot data :
+############
+
+def get_ped_table(file_list):
     
+    bline_table = None
     
+    for dl1file in sorted(file_list):
+        if bline_table is None :
+            try:
+                bline_table = read_table(dl1file,
+                                         '/dl1/monitoring/telescope/pedestal')
+            except:
+                print("pedestal not found in {}".format(dl1file))
+        else :
+            try:
+                bline_table = astropy.table.vstack([bline_table,
+                                                   read_table(dl1file,
+                                                              '/dl1/monitoring/telescope/pedestal')])
+            except:
+                print("pedestal not found in {}".format(dl1file))
+    return bline_table
+
+def get_ped_table_low_res(file_list):
+    
+    bline_table = None
+    
+    for dl1file in sorted(file_list):
+        if bline_table is None :
+            try:
+                bline_table = read_table(dl1file,
+                                         '/dl1/monitoring/telescope/pedestal')[0]
+            except:
+                print("pedestal not found in {}".format(dl1file))
+        else :
+            try:
+                bline_table = astropy.table.vstack([bline_table,
+                                                   read_table(dl1file,
+                                                              '/dl1/monitoring/telescope/pedestal')[0]])
+            except:
+                print("pedestal not found in {}".format(dl1file))
+    return bline_table
+
+
+def plot_average_nsb_VS_time(ped_table,ntel,ax=None):
+    NSB = VAR_to_NSB(ped_table['pedestal_charge_std'].mean(axis=1)**2,ntel)
+    Dates = [Time(t,scale='utc',format='unix').to_datetime() for t in  ped_table['pedestal_sample_time']]
+    if ax is None:
+        f,ax = plt.subplots(figsize=(10,5))
+    ax.plot(Dates,NSB,'.',label='tel {}'.format(ntel%20))
+    plt.xlabel('Time')
+    plt.ylabel('NSB photons [MHz]')
+    return ax
+
+def plot_average_nsb_photon_rate_VS_time(ped_table,ntel,ax=None):
+    NSB = VAR_to_NSB_photon_rate(ped_table['pedestal_charge_std'].mean(axis=1)**2,ntel)
+    Dates = [Time(t,scale='utc',format='unix').to_datetime() for t in  ped_table['pedestal_sample_time']]
+    if ax is None:
+        f,ax = plt.subplots(figsize=(10,5))
+    ax.plot(Dates,NSB,'.',label='tel {}'.format(ntel%20))
+    plt.xlabel('Time')
+    plt.ylabel('NSB [MHz]')
+    return ax
