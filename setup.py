5399f8f7fe578a25f123eb32b90db66c9df52cca
Jakub Jurysek <jakubjurysek@astronomie,cz>
2024-08-23 13:11:02 +0200

    entry point added

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
             'rate_scans_extraction = sst1mpipe.scripts.rate_scans_extraction:main',
             'create_hdu_indexes = sst1mpipe.scripts.create_hdu_indexes:main',
             'sst1mpipe_extract_dl1_distributions = sst1mpipe.scripts.sst1mpipe_extract_dl1_distributions:main'
+            'sst1mpipe_night_summary = sst1mpipe.scripts.sst1mpipe_night_summary:main'
         ]
     }
 )
