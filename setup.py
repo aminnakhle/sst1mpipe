96a0abfa35df2d5b77374856967a1f77d3d2077d
Jakub Jurysek <jakubjurysek@astronomie,cz>
2024-09-03 18:49:16 +0200

    bugfix

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
             'sst1mpipe_pointing_check = sst1mpipe.scripts.sst1mpipe_pointing_check:main',
             'rate_scans_extraction = sst1mpipe.scripts.rate_scans_extraction:main',
             'create_hdu_indexes = sst1mpipe.scripts.create_hdu_indexes:main',
-            'sst1mpipe_extract_dl1_distributions = sst1mpipe.scripts.sst1mpipe_extract_dl1_distributions:main'
+            'sst1mpipe_extract_dl1_distributions = sst1mpipe.scripts.sst1mpipe_extract_dl1_distributions:main',
             'sst1mpipe_night_summary = sst1mpipe.scripts.sst1mpipe_night_summary:main'
         ]
     }
